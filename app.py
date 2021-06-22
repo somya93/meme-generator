97e65445e24f41dae78b4a1eeb2becb1001413e0
somya93 <somya93@gmail.com>
2021-06-22 18:22:31 +0530

    Integrated Flask

--- app.py
+++ app.py
@@ -16,21 +16,58 @@
 
 """Overlays meme glasses on detected faces in the given image."""
 
-import argparse
 import math
 import requests
+
+from flask import Flask, request, jsonify
 from google.cloud import vision
 from PIL import Image, ImageDraw
 from io import BytesIO
 
+app = Flask(__name__)
+
+
+@app.route('/generatememe', methods=["POST"])
+def index():
+    if request.method == "POST":
+        data = request.get_json()
+        generateMeme(data)
+        return jsonify("OK"), 200
+
+@app.route('/')
+def home():
+    return jsonify("GET OK"), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+def generateMeme(request_data):
+    uri = request_data["uri"]
+    output_filename = "out.jpg"
+    faces = detect_face(uri)
+    print('Found {} face{}'.format(
+        len(faces), '' if len(faces) == 1 else 's'))
+    print('Writing to file {}'.format(output_filename))
+    # Reset the file pointer, so we can read the file again
+    # image.seek(0)
+
+    # get the eyes' landmarks
+    response = requests.get(uri)
+    image = Image.open(BytesIO(response.content))
+    box = highlight_eyes(image, faces, output_filename)
+
+    # overlay the meme glasses image onto the face
+    with open("resources\meme_glasses.png", 'rb') as image2:
+        add_prop(output_filename, image2, output_filename, box)
+
 
 # [START vision_face_detection_tutorial_send_request]
-def detect_face(face_file, max_results):
+def detect_face(face_file):
     """Uses the Vision API to detect faces in the given file.
 
     Args:
         face_file: A file-like object containing an image with faces.
-        max_results: Maximum number of faces to be detected
 
     Returns:
         An array of Face objects with information about the picture.
@@ -43,7 +80,7 @@ def detect_face(face_file, max_results):
     image = vision.Image()
     image.source.image_uri = face_file
     return client.face_detection(
-        image=image, max_results=max_results).face_annotations
+        image=image).face_annotations
 
 
 # [END vision_face_detection_tutorial_send_request]
@@ -90,6 +127,7 @@ def highlight_eyes(im, faces, output_filename):
 
 # [END vision_face_detection_tutorial_process_response]
 
+
 # [START]
 def add_prop(face_image, prop_image, output_file, box):
     background = Image.open(face_image)
@@ -133,43 +171,4 @@ def add_prop(face_image, prop_image, output_file, box):
     background.show()
     background.save(output_file)
 
-
 # [END]
-
-# [START vision_face_detection_tutorial_run_application]
-def main(input_filename, output_filename, max_results):
-    # with open(input_filename, 'rb') as image:
-    faces = detect_face(input_filename, max_results)
-    print('Found {} face{}'.format(
-        len(faces), '' if len(faces) == 1 else 's'))
-    print('Writing to file {}'.format(output_filename))
-    # Reset the file pointer, so we can read the file again
-    # image.seek(0)
-
-    # get the eyes' landmarks
-    response = requests.get(input_filename)
-    image = Image.open(BytesIO(response.content))
-    box = highlight_eyes(image, faces, output_filename)
-
-    # overlay the meme glasses image onto the face
-    with open("resources\meme_glasses.png", 'rb') as image2:
-        add_prop(output_filename, image2, output_filename, box)
-
-
-# [END vision_face_detection_tutorial_run_application]
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Detects faces in the given image.')
-    parser.add_argument(
-        'input_image', help='the image you\'d like to detect faces in.')
-    parser.add_argument(
-        '--out', dest='output', default='out.jpg',
-        help='the name of the output file.')
-    parser.add_argument(
-        '--max-results', dest='max_results', default=4,
-        help='the max results of face detection.')
-    args = parser.parse_args()
-
-    main(args.input_image, args.output, args.max_results)
